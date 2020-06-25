564f38f6ad685984000b628768d4dc2ec545677d
William Koch <47284092+wak31415@users.noreply.github.com>
2020-06-25 15:37:20 +0200

    Prepare for icon use

--- form.php
+++ form.php
@@ -28,6 +28,7 @@
     <script src="js/general_scripts.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <title>SecureForms</title>
 </head>
