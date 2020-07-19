fc2ecab0910ee0cc4bcbbca4fdefaaa30679dd81
William Koch <47284092+wak31415@users.noreply.github.com>
2020-07-19 13:53:25 +0200

    Only display sign up prompt if not signed in

--- thank_you.php
+++ thank_you.php
@@ -8,16 +8,16 @@
         <p>Your response has been successfully encrypted and recorded.</p>
     </div>
 
-    <div class="jumbotron text-center">
     <?php
         if (!isset($_SESSION['email'])) {
             echo
             '
+            <div class="jumbotron text-center">
             <h1>Start creating your own free and secure forms today!</h1>
             <p>Easy to use, end-to-end encrypted, open source and free... What more could you want?</p>
             <a href="register.php" class="btn btn-success btn-lg" role="button">Register</a>
+            </div>
             ';
         }
     ?>
-    </div>
 </div>
