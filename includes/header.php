31be20fccee5a01726eaf48b3e3dfc4d666a1e01
William Koch <47284092+wak31415@users.noreply.github.com>
2020-07-31 16:03:18 +0200

    Remove unused font

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,6 @@
     <script src="js/general_scripts.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/genome" type="text/css"/> 
     <link rel="stylesheet" href="style.css">
     <title>SecureForms</title>
 </head>
